df009c60adb2c990d123ca62828240247dca1718
baepaul <paul@gitdot.io>
2026-01-26 18:29:18 -0500

    max width

--- frontend/app/[owner]/[repo]/page.tsx
+++ frontend/app/[owner]/[repo]/page.tsx
@@ -15,7 +15,7 @@ export default async function Page({
     return <div>README.md not found</div>;
   }
 
-  return <div className="p-4">
+  return <div className="p-4 max-w-4xl">
     <MarkdownBody content={readme.content} />
   </div>;
 }
