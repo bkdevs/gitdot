771285b63c4302eb3b9933a5e119ec2b2024c7d8
baepaul <paul@gitdot.io>
2026-01-27 12:57:13 -0500

    overflow nit

--- frontend/app/[owner]/[repo]/questions/[id]/page.tsx
+++ frontend/app/[owner]/[repo]/questions/[id]/page.tsx
@@ -4,7 +4,7 @@ import { Question } from "./ui/question";
 export default function Page({ params }: { params: { id: string } }) {
   return (
     <div className="flex flex-col w-full h-screen">
-      <div className="flex-1 flex-col overflow-y-auto scrollbar-thin pb-20">
+      <div className="flex-1 flex-col pb-20">
         <Question />
         <Answers />
       </div>
