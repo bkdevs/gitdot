9fecdae93ccc8565ff31ba5891f73f54eeedaeee
baepaul <paul@gitdot.io>
2026-01-27 17:14:52 -0500

    vote box

--- frontend/app/[owner]/[repo]/questions/[id]/ui/vote-box.tsx
+++ frontend/app/[owner]/[repo]/questions/[id]/ui/vote-box.tsx
@@ -1,15 +1,29 @@
-import { ChevronUp, ChevronDown } from "lucide-react";
-
 export function VoteBox({ score }: { score: number }) {
   return (
-    <div className="flex flex-col items-center gap-0 text-muted-foreground text-xl">
-      <button className="text-muted-foreground hover:text-foreground">
-        <ChevronUp />
+    <div className="flex flex-col items-center gap-1 text-muted-foreground text-xl">
+      <button className="text-muted-foreground hover:text-foreground cursor-pointer">
+        <TriangleUp />
       </button>
       <span>{score}</span>
-      <button className="text-muted-foreground hover:text-foreground">
-        <ChevronDown />
+      <button className="text-muted-foreground hover:text-foreground cursor-pointer">
+        <TriangleDown />
       </button>
     </div>
+  );
+}
+
+function TriangleUp() {
+  return (
+    <svg width="16.971" height="12" viewBox="0 0 16.971 12">
+      <polygon points="8.485,0 0,12 16.971,12" fill="currentColor" />
+    </svg>
+  );
+}
+
+function TriangleDown() {
+  return (
+    <svg width="16.971" height="12" viewBox="0 0 16.971 12">
+      <polygon points="0,0 16.971,0 8.485,12" fill="currentColor" />
+    </svg>
   );
 }
