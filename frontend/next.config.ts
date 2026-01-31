e4e1ffe6665ef6dd2f9d0e982630ccbed50e6ca3
baepaul <paul@gitdot.io>
2026-01-31 11:44:20 -0500

    adding /beta redirect

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
+  async redirects() {
+    return [
+      {
+        source: "/beta",
+        destination: "/bkdevs/gitdot",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
