1433cf78e58d9bc6103c22184abc739780294353
mikkel <mikkel@gitdot.io>
2026-02-01 21:08:09 -0500

    implemented a middleware for git http endpoints to attach www-authenticate header in response when unauthorized

--- backend/src/handler/git_http.rs
+++ backend/src/handler/git_http.rs
@@ -4,6 +4,10 @@ mod git_upload_pack;
 
 use axum::{
     Router,
+    body::Body,
+    http::{Request, StatusCode, header},
+    middleware::{self, Next},
+    response::Response,
     routing::{get, post},
 };
 
@@ -18,4 +22,18 @@ pub fn create_git_http_router() -> Router<AppState> {
         .route("/{owner}/{repo}/info/refs", get(git_info_refs))
         .route("/{owner}/{repo}/git-upload-pack", post(git_upload_pack))
         .route("/{owner}/{repo}/git-receive-pack", post(git_receive_pack))
+        .layer(middleware::from_fn(add_www_authenticate_header))
+}
+
+/// Middleware to add the WWW-Authenticate header to the response as
+/// git expects a WWW-Authenticate header when unauthorized.
+async fn add_www_authenticate_header(request: Request<Body>, next: Next) -> Response {
+    let mut response = next.run(request).await;
+    if response.status() == StatusCode::UNAUTHORIZED {
+        response.headers_mut().insert(
+            header::WWW_AUTHENTICATE,
+            "Basic realm=\"gitdot\"".parse().unwrap(),
+        );
+    }
+    response
 }
