df8e54a21e15f1aa193750aeb3257d3fd7f7a7c7
mck782 <mikkel@gitdot.io>
2026-02-06 15:26:57 -0500

    chained user router

--- backend/src/handler/user.rs
+++ backend/src/handler/user.rs
@@ -18,8 +18,7 @@ use validate_name::validate_name;
 pub fn create_user_router() -> Router<AppState> {
     Router::new()
         .route("/user", get(get_current_user))
-        .route("/user/{user_name}", get(get_user))
-        .route("/user/{user_name}", head(validate_name))
+        .route("/user/{user_name}", get(get_user).head(validate_name))
         .route(
             "/user/{user_name}/repositories",
             get(list_user_repositories),
