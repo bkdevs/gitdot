6e029ded07a31d05c3f7f925dd0d801dff1f7237
baepaul <paul@gitdot.io>
2026-02-12 19:17:15 -0500

    lost mikkel's change

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
+  experimental: {
+    proxyClientMaxBodySize: "2gb",
+  },
   async redirects() {
     return [
       {
