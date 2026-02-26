419cd17da90efd39a7969143c8bd1592389d75cf
baepaul <paul@gitdot.io>
2026-02-26 13:20:03 -0500

    just  going to use http sse, no sdk required

--- s2-sdk-typescript/src/tests/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, it } from "vitest";
-import { AppendInput, AppendRecord } from "../index.js";
-
-describe("S2", () => {
-	it("should succeed", () => {
-		expect(true).toBe(true);
-	});
-});
