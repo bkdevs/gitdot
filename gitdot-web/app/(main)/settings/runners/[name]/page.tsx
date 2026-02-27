73be7bb6c7ef482e12006c7829f976cacf8ca0e4
baepaul <paul@gitdot.io>
2026-02-27 08:46:23 -0500

    back to starting at pixels

--- gitdot-web/app/(main)/settings/runners/[name]/page.tsx
+++ gitdot-web/app/(main)/settings/runners/[name]/page.tsx
@@ -1,9 +1,9 @@
 import { notFound } from "next/navigation";
 
 import { getCurrentUser, getRunner } from "@/dal";
-import { formatDateTime, timeAgoFull } from "@/util";
 import { InstallRunnerForm } from "./ui/install-runner-form";
-import { RunnerStatus } from "./ui/runner-status";
+import { RunnerDetails } from "./ui/runner-details";
+import { RunnerMain } from "./ui/runner-main";
 
 export default async function Page({
   params,
@@ -21,28 +21,17 @@ export default async function Page({
     return <InstallRunnerForm runner={runner} ownerName={user.name} />;
   }
 
-  const createdAt = new Date(runner.created_at);
-  const lastActive = new Date(runner.last_active);
-  const isActive = Date.now() - lastActive.getTime() <= 90 * 1000;
-
   return (
-    <div>
+    <div className="flex flex-col w-full">
       <h1 className="flex items-center border-b border-border pl-2 h-9">
         {runner.name}
       </h1>
-      <div className="p-2">
-        <ul className="text-sm text-muted-foreground space-y-1">
-          <li>
-            <RunnerStatus runner={runner} />
-          </li>
-          <li>
-            <span>Owner:</span> {runner.owner_name}</li>
-          <li>
-            <span>Created:</span>{" "}
-            {formatDateTime(createdAt)}
-          </li>
-        </ul>
+      <div className="flex w-full">
+        <RunnerMain />
+        <RunnerDetails runner={runner} />
       </div>
+      <div className="flex border-t border-border" />
+
     </div>
   );
 }
--- gitdot-web/app/(main)/settings/runners/ui/runners.tsx
+++ gitdot-web/app/(main)/settings/runners/ui/runners.tsx
@@ -2,7 +2,6 @@ import Link from "@/ui/link";
 import { timeAgoFull } from "@/util";
 import type { RunnerResource } from "gitdot-api";
 import { Plus } from "lucide-react";
-import { RunnerStatus } from "../[name]/ui/runner-status";
 
 export function Runners({ runners }: { runners: RunnerResource[] }) {
   return (
@@ -29,6 +28,22 @@ export function Runners({ runners }: { runners: RunnerResource[] }) {
       )}
     </div>
   );
+}
+
+function RunnerStatus({ runner }: { runner: RunnerResource }) {
+  if (!runner.last_active) {
+    return <span className="text-amber-600">Pending installation</span>;
+  }
+  const lastActiveDate = new Date(runner.last_active);
+
+  // heuristic to account for the touch in /task/poll
+  const isActive =
+    Date.now() - lastActiveDate.getTime() <= 90 * 1000;
+  if (isActive) {
+    return <span className="text-green-600">Active</span>;
+  }
+
+  return <span>Active {timeAgoFull(lastActiveDate)}</span>;
 }
 
 function RunnersHeader() {
