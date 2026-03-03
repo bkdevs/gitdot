7984dac7fe4ef171da89735db81b965ead3fd7a7
baepaul <paul@gitdot.io>
2026-03-02 21:10:56 -0500

    moving api-ts under gitdot-web to keep vercel happy :(

--- gitdot-web/packages/api/src/endpoint/repository/get-repository-file.ts
+++ gitdot-web/packages/api/src/endpoint/repository/get-repository-file.ts
@@ -3,20 +3,20 @@ import { RepositoryFileResource } from "../../resource";
 import type { Endpoint } from "../endpoint";
 
 export const GetRepositoryFileRequest = z.object({
-	path: z.string(),
-	ref_name: z.string().optional(),
+  path: z.string(),
+  ref_name: z.string().optional(),
 });
 export type GetRepositoryFileRequest = z.infer<typeof GetRepositoryFileRequest>;
 
 export const GetRepositoryFileResponse = RepositoryFileResource;
 export type GetRepositoryFileResponse = z.infer<
-	typeof GetRepositoryFileResponse
+  typeof GetRepositoryFileResponse
 >;
 
 export const GetRepositoryFile = {
-	path: "/repository/{owner}/{repo}/file",
-	method: "GET",
-	request: GetRepositoryFileRequest,
-	response: GetRepositoryFileResponse,
+  path: "/repository/{owner}/{repo}/file",
+  method: "GET",
+  request: GetRepositoryFileRequest,
+  response: GetRepositoryFileResponse,
 } as const satisfies Endpoint;
 export type GetRepositoryFile = typeof GetRepositoryFile;
