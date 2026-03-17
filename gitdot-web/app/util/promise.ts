c86c8ca931e0307be5be50e48479aa2f5d2f762e
baepaul <paul@gitdot.io>
2026-03-17 06:26:13 -0400

    fixing ssr all null error

--- gitdot-web/app/util/promise.ts
+++ gitdot-web/app/util/promise.ts
@@ -4,10 +4,11 @@ export function delay<T>(ms: number, value: T): Promise<T> {
 
 export function firstNonNull<T>(
   ...promises: Promise<T | null | undefined>[]
-): Promise<T> {
-  return new Promise((resolve, reject) => {
+): Promise<T | null> {
+  return new Promise((resolve) => {
     let remaining = promises.length;
     let resolved = false;
+
     for (const p of promises) {
       Promise.resolve(p)
         .then((value) => {
@@ -16,12 +17,12 @@ export function firstNonNull<T>(
             resolved = true;
             resolve(value);
           } else if (--remaining === 0) {
-            reject(new Error("all null"));
+            resolve(null);
           }
         })
         .catch(() => {
           if (resolved) return;
-          if (--remaining === 0) reject(new Error("all null"));
+          if (--remaining === 0) resolve(null);
         });
     }
   });
