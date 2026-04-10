5d52101e54c0ea6b94adbd81b56ce7c32b1843a0
baepaul <paul@gitdot.io>
2026-04-10 18:29:41 -0400

    skip.

--- gitdot-web/packages/api/src/endpoint/user/upload-user-image.ts
+++ gitdot-web/packages/api/src/endpoint/user/upload-user-image.ts
@@ -1,14 +1,10 @@
 import { z } from "zod";
-import { UploadUserImageResource } from "../../resource";
 import type { Endpoint } from "../endpoint";
-
-export const UploadUserImageResponse = UploadUserImageResource;
-export type UploadUserImageResponse = z.infer<typeof UploadUserImageResponse>;
 
 export const UploadUserImage = {
   path: "/user/image",
   method: "POST",
   request: z.instanceof(Blob),
-  response: UploadUserImageResponse,
+  response: z.void(),
 } as const satisfies Endpoint;
 export type UploadUserImage = typeof UploadUserImage;
