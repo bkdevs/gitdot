0c334bb9f30634925cd841c4ca200c495f847a7e
baepaul <paul@gitdot.io>
2026-04-13 09:57:29 -0400

    deleting more dead code

--- gitdot-web/app/(auth)/onboarding/page.tsx
+++
@@ -1,9 +0,0 @@
-import UsernameForm from "./ui/username-form";
-
-export default function Page() {
-  return (
-    <div className="max-w-3xl mx-auto flex gap-4 items-center justify-center h-screen">
-      <UsernameForm />
-    </div>
-  );
-}
