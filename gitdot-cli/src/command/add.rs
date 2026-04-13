4151499824706f08b210ead81233a6dfecf7b209
baepaul <paul@gitdot.io>
2026-04-13 17:32:16 -0400

    add command stripping readme

--- gitdot-cli/src/command/add.rs
+++ gitdot-cli/src/command/add.rs
@@ -5,14 +5,13 @@ use crate::git::GitWrapper;
 #[derive(Args, Debug)]
 pub struct AddArgs {
     /// Commit message
-    message: Option<String>,
+    message: String,
 }
 
 impl AddArgs {
     pub async fn execute(&self) -> anyhow::Result<()> {
         let git = GitWrapper::new();
         git.add_all().await?;
-        git.commit_allow_empty(self.message.as_deref().unwrap_or(""))
-            .await
+        git.commit_allow_empty(&self.message).await
     }
 }
