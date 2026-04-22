a6d24b144e3000387da97895c6c045ae0bcdc137
baepaul <paul@gitdot.io>
2026-04-21 20:06:13 -0400

    nit

--- gitdot-web/app/(main)/[owner]/[repo]/reviews/[number]/ui/review-diff-file-bubbles.tsx
+++ gitdot-web/app/(main)/[owner]/[repo]/reviews/[number]/ui/review-diff-file-bubbles.tsx
@@ -63,7 +63,7 @@ function ReviewDiffFileBubble({
       ref={ref}
       id={`comment-${thread.comments[0].id}`}
       className={cn(
-        "absolute z-50 flex flex-row items-center gap-1.5 px-2 py-0.5 bg-background border border-border rounded-full animate-in fade-in duration-200 hover:bg-accent",
+        "absolute z-50 flex flex-row items-center gap-1.5 px-2 py-0.5 bg-background border border-border rounded-full animate-in fade-in duration-200 hover:bg-accent select-none",
         side === "old" ? "right-full mr-2" : "left-full ml-2",
         isActive && "bg-accent",
       )}
@@ -75,7 +75,7 @@ function ReviewDiffFileBubble({
       <UserImage userId={thread.comments[0].author_id} px={16} />
       <span
         className={cn(
-          "text-xs font-sans select-none",
+          "text-xs font-sans",
           isActive ? "text-foreground" : "text-muted-foreground",
         )}
       >
